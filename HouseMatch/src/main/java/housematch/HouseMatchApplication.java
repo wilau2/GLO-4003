e83c3c07c9f187129b8dfa8a2c6816f63910f49f
wilau2 <william.lauze@gmail.com>
2015-09-09 16:55:08 -0400

    Test spring template, this is a springboot project, I am trying to talk with the teacher so we can keep this format To run it Left click HouseMatchApplication.java then run java app or spring boot

---
+++ HouseMatch/src/main/java/housematch/HouseMatchApplication.java
@@ -0,0 +1,15 @@
+package housematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HouseMatchApplication {
+	
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseMatchApplication.class, args);
+    }
+}
