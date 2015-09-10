9fce725aa7d6366c71a7777f8dfcb8957ffcfea5
Antoine Lefrançois <antoine.lefrancois.1@ulaval.ca>
2015-09-10 18:57:10 -0400

    Added checkstyle formatting in project

--- HouseMatch/src/main/java/housematch/HouseMatchApplication.java
+++ HouseMatch/src/main/java/housematch/HouseMatchApplication.java
@@ -3,13 +3,10 @@ package housematch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class HouseMatchApplication {
-	
 
-
-    public static void main(String[] args) {
-        SpringApplication.run(HouseMatchApplication.class, args);
-    }
+   public static void main(String[] args) {
+      SpringApplication.run(HouseMatchApplication.class, args);
+   }
 }
--- HouseMatch/src/test/java/housematch/HouseMatchApplicationTests.java
+++ HouseMatch/src/test/java/housematch/HouseMatchApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringApplicationConfiguration(classes = HouseMatchApplication.class)
 public class HouseMatchApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+   @Test
+   public void contextLoads() {
+   }
 
 }
