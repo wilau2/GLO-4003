0407c2a1c411b22bce287fa45e72096220902e71
Isaac <provenchez@gmail.com>
2015-09-19 13:42:55 -0400

    Ajout tes add and check sur les estates avant le persist

--- HouseMatch/src/main/java/ca/ulaval/glo4003/b6/housematch/estates/repository/XMLEstateRepository.java
+++ HouseMatch/src/main/java/ca/ulaval/glo4003/b6/housematch/estates/repository/XMLEstateRepository.java
@@ -1,6 +1,7 @@
 package ca.ulaval.glo4003.b6.housematch.estates.repository;
 
 import java.util.Collection;
+import java.util.HashMap;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -9,6 +10,8 @@ import ca.ulaval.glo4003.b6.housematch.estates.domain.Estate;
 import ca.ulaval.glo4003.b6.housematch.persistance.XMLFileEditor;
 
 public class XMLEstateRepository implements EstateRepository{
+
+   private static final String ESTATE = "estate";
 
    private XMLFileEditor xmlFileEditor;
    
@@ -24,10 +27,33 @@ public class XMLEstateRepository implements EstateRepository{
    public void addEstate(Estate estate) {
       try {
          Document estateDocument = xmlFileEditor.readXMLFile(XML_FILE_PATH);
+         HashMap<String, String> attributes = createHashMapFromEstate(estate);
+         if(isEstatePersisted(estateDocument,attributes)){
+            return;
+         }
+         addNewEstateToDocument(estateDocument, attributes);
       } catch (DocumentException e) {
-         // TODO Auto-generated catch block
+         
          e.printStackTrace();
       }
+   }
+
+   private boolean isEstatePersisted(Document existingDocument, HashMap<String, String> attributes) {
+      return xmlFileEditor.elementWithCorrespondingValuesExists(existingDocument, "estates/estate/price", attributes.get("price"));
+   }
+
+   public HashMap<String, String> createHashMapFromEstate(Estate estate) {
+      HashMap<String, String> attributes = new HashMap<String, String>();
+
+      attributes.put("type", estate.getType());
+      attributes.put("address", estate.getAddress());
+      attributes.put("price", estate.getPrice().toString());
+      
+      return attributes;
+   }
+
+   private void addNewEstateToDocument(Document document, HashMap<String, String> attributes) {
+      xmlFileEditor.addNewElementToDocument(document, ESTATE, attributes);
    }
 
 }
