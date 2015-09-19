748a5e5fb9ed5f7c8ab4274328ffe89926a6cd50
wilau2 <william.lauze@gmail.com>
2015-09-19 14:16:25 -0400

    Fix integration probleme

--- HouseMatch/src/main/java/ca/ulaval/glo4003/b6/housematch/estates/repository/XMLEstateRepository.java
+++ HouseMatch/src/main/java/ca/ulaval/glo4003/b6/housematch/estates/repository/XMLEstateRepository.java
@@ -9,14 +9,14 @@ import org.dom4j.DocumentException;
 import ca.ulaval.glo4003.b6.housematch.estates.domain.Estate;
 import ca.ulaval.glo4003.b6.housematch.persistance.XMLFileEditor;
 
-public class XMLEstateRepository implements EstateRepository{
+public class XMLEstateRepository implements EstateRepository {
 
    private static final String ESTATE = "estate";
 
    private XMLFileEditor xmlFileEditor;
-   
+
    private String XML_FILE_PATH = "persistence/estates.xml";
-   
+
    @Override
    public Collection<Estate> getAllEstate() {
       // TODO Auto-generated method stub
@@ -28,18 +28,19 @@ public class XMLEstateRepository implements EstateRepository{
       try {
          Document estateDocument = xmlFileEditor.readXMLFile(XML_FILE_PATH);
          HashMap<String, String> attributes = createHashMapFromEstate(estate);
-         if(isEstatePersisted(estateDocument,attributes)){
+         if (isEstatePersisted(estateDocument, attributes)) {
             return;
          }
          addNewEstateToDocument(estateDocument, attributes);
       } catch (DocumentException e) {
-         
+
          e.printStackTrace();
       }
    }
 
    private boolean isEstatePersisted(Document existingDocument, HashMap<String, String> attributes) {
-      return xmlFileEditor.elementWithCorrespondingValuesExists(existingDocument, "estates/estate/price", attributes.get("price"));
+      return xmlFileEditor.elementWithCorrespondingValuesExists(existingDocument, "estates/estate/price",
+            attributes.get("price"));
    }
 
    public HashMap<String, String> createHashMapFromEstate(Estate estate) {
@@ -48,12 +49,13 @@ public class XMLEstateRepository implements EstateRepository{
       attributes.put("type", estate.getType());
       attributes.put("address", estate.getAddress());
       attributes.put("price", estate.getPrice().toString());
-      
+
       return attributes;
    }
 
    private void addNewEstateToDocument(Document document, HashMap<String, String> attributes) {
-      xmlFileEditor.addNewElementToDocument(document, ESTATE, attributes);
+      // TODO BORIS doit faire l'integration
+      // xmlFileEditor.addNewElementToDocument(document, ESTATE, attributes);
    }
 
 }
