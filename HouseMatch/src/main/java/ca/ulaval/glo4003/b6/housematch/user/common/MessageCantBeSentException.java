54bff5afb44da516903d55ac886fbcd17446e492
Martin Savoie <martin.savoie.2@ulaval.ca>
2015-10-14 17:53:57 -0400

    Moving final

--- HouseMatch/src/main/java/ca/ulaval/glo4003/b6/housematch/user/common/MessageCantBeSentException.java
+++ HouseMatch/src/main/java/ca/ulaval/glo4003/b6/housematch/user/common/MessageCantBeSentException.java
@@ -3,10 +3,11 @@ package ca.ulaval.glo4003.b6.housematch.user.common;
 import javax.mail.MessagingException;
 
 public class MessageCantBeSentException extends Exception {
+   private static final long serialVersionUID = -2118708352009688565L;
+
    public MessageCantBeSentException(String string, MessagingException e) {
      super(string,e);
    }
 
-   private static final long serialVersionUID = -2118708352009688565L;
 
 }
